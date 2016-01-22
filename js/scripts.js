603151bc16d732321251e19fc958a2100d3acbb0
Guest User <student@epicodus.com>
2016-01-22 11:13:11 -0800

    update scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,27 @@
+$(document).ready(function() {
+  $("form#pongForm").submit(function() {
+    event.preventDefault();
+    var number = parseInt($("input#number").val());
+
+    var resultArray = returnRange(number);
+    var result = resultArray.toString();
+
+    $("#result").append(result);
+  })
+  });
+
+
 var returnRange = function (number) {
   var output = [];
   for (var i = 1; i <= number; i++) {
     output.push(i);
+
     }
 
   return output;
+
 };
+
 
 var divisThree = function (number) {
   if (number %3 === 0) {
@@ -24,114 +40,3 @@ var divisTeen = function (number) {
     return "pingpong";
   }
 };
-
-
-
-
-
-$(document).ready(function() {
-  $("form#pongForm").submit(function() {
-    event.preventDefault();
-    var number = parseInt($("input#number").val());
-
-    var resultArray = returnRange(number);
-    var result = resultArray.toString();
-
-    $("#result").append(result);
-  })
-
-
-  });
-
-
-
-
-
-
-
-//
-// var pigLatin = function(word){
-//   if (isVowel(word)) {
-//     return addAy(word)
-//   } else {
-//     return moveCons(word)
-//     return addAy3(word)
-//   }
-// }
-//
-//
-// var isVowel = function(word) {
-//   if (word[0].match(/[aeiou]/)) {
-//     return true;
-//   }
-//   return false;
-// };
-//
-// var isCons = function(word) {
-//   if ((word[0] !=~ /[aeiou]/) && (word[1] !=~ /[aeiou]/) && (word[2] !=~ /[aeiou]/)) {
-//     return "two consonants";
-//   }
-//   if ((word[0] !=~ /[aeiou]/) && (word[1] !=~ /[aeiou]/)) {
-//     return "two consonants";
-//   }
-//   else if (word[0] !=~ /[aeiou]/) {
-//     return "one consonant";
-//   }
-// }
-//
-// var addAy = function (word) {
-//   if (isVowel(word)) {
-//     return (word.concat("ay"));
-//   }
-// };
-//
-// var moveCons = function (word) {
-//
-//   if (isCons(word) === "two consonants") {
-//     console.log(isCons(word))
-//     var string = word.slice(0,2);
-//     word = word.slice(2);
-//     return((word.concat(string))+("ay"));
-//   }
-//   if (isCons(word) ) {
-//     console.log("three consonants!")
-//
-//     var string = word.slice(0,3);
-//     word = word.slice(3);
-//     return((word.concat(string))+("ay"));
-//   }
-//   if (isCons(word) === "one consonant") {
-//     console.log("one consonants!")
-//
-//     var string = word.slice(0,1);
-//     word = word.slice(1);
-//     return((word.concat(string))+("ay"));
-//     }
-//   };
-//
-// var addAy3 = function (word) {
-//
-//   if (isCons(word)) {
-//     var string = word.slice(0,2);
-//     word = word.slice(2);
-//     return((word.concat(string))+("ay"));
-//   }
-//
-//   if (isCons(word)) {
-//     var string = word.slice(0,3);
-//     word = word.slice(3);
-//     return((word.concat(string))+("ay"));
-//   }
-// };
-//
-// $(document).ready(function() {
-//   $("form#pigLatinForm").submit(function(event) {
-//     var word = $("input#word").val();
-//     var result = pigLatin(word);
-//
-//
-//     $("#result").append(result);
-//     event.preventDefault();
-//   });
-// });
-//
