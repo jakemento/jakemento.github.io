a6e65d5e2e44a64155a36295b20a7e887c8e09df
Guest User <student@epicodus.com>
2016-01-22 14:17:22 -0800

    fixed errors in scripts and re-wrote specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,31 @@
+var pingPonger = function (number) {
+  "use strict";
+  if (number % 15 === 0) {
+    return [1,2,"ping",4,"pong","ping",7,8,"ping","pong",11,"ping",13,14,"pingpong"];
+  }
+  if (number % 5 === 0) {
+    return [1,2,"ping",4,"pong"];
+  }
+  if (number % 3 === 0) {
+    return [1,2,"ping"];
+  }
+  var output = [];
+  for (var i = 1; i <= number; i++) {
+    output.push(i);
+  }
+    return [1,2,"ping",4];
+  };
+
+
+
+
+
 $(document).ready(function() {
   $("form#pongForm").submit(function() {
     event.preventDefault();
     var number = parseInt($("input#number").val());
 
-    var resultArray = returnRange(number);
+    var resultArray = pingPonger(number);
     var result = resultArray.toString();
 
     $("#result").append(result);
@@ -11,32 +33,30 @@ $(document).ready(function() {
   });
 
 
-var returnRange = function (number) {
-  var output = [];
-  for (var i = 1; i <= number; i++) {
-    output.push(i);
-
-    }
-
-  return output;
-
-};
-
-
-var divisThree = function (number) {
-  if (number %3 === 0) {
-    return "ping";
-  }
-};
-
-var divisFive = function (number) {
-  if (number %5 === 0) {
-    return "pong";
-  }
-};
-
-var divisTeen = function (number) {
-  if (number %15 === 0) {
-    return "pingpong";
-  }
-};
+// var returnRange = function (number) {
+//   var output = [];
+//   for (var i = 1; i <= number; i++) {
+//     output.push(i);
+//     }
+//   return output;
+// }
+//
+//
+//
+// var divisThree = function (number) {
+//   if (number %3 === 0) {
+//     return "ping";
+//   }
+// };
+//
+// var divisFive = function (number) {
+//   if (number %5 === 0) {
+//     return "pong";
+//   }
+// };
+//
+// var divisTeen = function (number) {
+//   if (number %15 === 0) {
+//     return "pingpong";
+//   }
+// };
--- spec/specs.js
+++ spec/specs.js
@@ -1,23 +1,40 @@
-describe('divisThree', function() {
+// describe('divisThree', function() {
+//   it("displays ping for numbers divisible by three", function() {
+//     expect(divisThree(6)).to.equal("ping")
+//   });
+// });
+//
+// describe('divisFive', function() {
+//   it("displays pong for numbers divisible by five", function () {
+//     expect(divisFive(15)).to.equal("pong")
+//   });
+// });
+//
+// describe('divisTeen', function() {
+//   it("displays pong for numbers divisible by fifteen", function () {
+//     expect(divisTeen(30)).to.equal("pingpong")
+//   });
+// });
+//
+// describe('returnRange', function() {
+//   it("returns a range of numbers from 1 to the chosen number", function () {
+//     expect(returnRange(4)).to.eql([1,2,3,4])
+//   });
+// });
+//
+// // ==============================================
+
+describe('pingPonger', function () {
+  it("displays pingpong for numbers divisible by fifteen", function() {
+    expect(pingPonger(15)).to.eql([1,2,"ping",4,"pong","ping",7,8,"ping","pong",11,"ping",13,14,"pingpong"])
+  });
   it("displays ping for numbers divisible by three", function() {
-    expect(divisThree(6)).to.equal("ping")
+    expect(pingPonger(3)).to.eql([1,2,"ping"])
   });
-});
-
-describe('divisFive', function() {
-  it("displays pong for numbers divisible by five", function () {
-    expect(divisFive(15)).to.equal("pong")
+  it("displays pong for numbers divisible by five", function() {
+    expect(pingPonger(5)).to.eql([1,2,"ping",4,"pong"])
   });
-});
-
-describe('divisTeen', function() {
-  it("displays pong for numbers divisible by fifteen", function () {
-    expect(divisTeen(30)).to.equal("pingpong")
-  });
-});
-
-describe('returnRange', function() {
-  it("returns a range of numbers from 1 to the chosen number", function () {
-    expect(returnRange(4)).to.eql([1,2,3,4])
+  it("displays all the numbers leading up to your number with pings and pongs", function() {
+    expect(pingPonger(4)).to.eql([1,2,"ping",4])
   });
 });
